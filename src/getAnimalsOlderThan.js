ff6aa4ac6bf5193ce945a46d0fa4e18dc4a1a966
cunhavcs <cunha.vcs@gmail.com>
2022-10-31 17:22:46 -0300

    Refatora requisito 02

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,9 +1,7 @@
 const data = require('../data/zoo_data');
 
-function getAnimalsOlderThan(animal, age) {
-  return data.species
-    .find((specie) => specie.name === animal).residents
-    .every((resident) => resident.age >= age);
-}
+const getAnimalsOlderThan = (animal, age) => data.species
+  .find((specie) => specie.name === animal).residents
+  .every((resident) => resident.age >= age);
 
 module.exports = getAnimalsOlderThan;
