8a6052375064738aa21e8dd6a40b8ce424ce8f00
cunhavcs <cunha.vcs@gmail.com>
2022-10-31 15:48:55 -0300

    Resolve requisito 02 - Implemente a função getAnimalsOlderThan

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,23 +1,7 @@
 const data = require('../data/zoo_data');
 
-// const { species } = data;
-// console.log(species);
+function getSpeciesByIds(ids) {
 
-// function getSpeciesByIds(ids) {
-//   for (let i = 0; i < species.length; i += 1) {
-//     if (ids === species[i].id) {
-//       return species[i];
-//     }
-//   }
-//   return 0;
-// }
-
-// function getSpeciesByIds(...ids) {
-//   return species.find((specie) => {
-//     return ids === specie.id;
-//   })
-// }
-
-// console.log(getSpeciesByIds('e8481c1d-42ea-4610-8e11-1752cfc05a46'));
+}
 
 module.exports = getSpeciesByIds;
