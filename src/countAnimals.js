be83999dc45fc166ef540f75b92b5b756a3991ea
cunhavcs <cunha.vcs@gmail.com>
2022-11-01 18:08:25 -0300

    Resolve requisitos 04 e 05 - implementa funções getRelatedEmployees e countAnimals

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,15 +1,23 @@
-// const data = require('../data/zoo_data');
+const data = require('../data/zoo_data');
 
-// const { species } = data;
+const { species } = data;
 
-// const countAnimals = (animal) => {
-//   const reducedEntries = Object.entries(animal).reduce((acc, curr) => acc.concat(curr));
-//   // reducedEntries = ['chave', 'valor']
-//   species
-// }
+const countAnimals = (animal) => {
+  if (animal === undefined) {
+    const animals = {};
+    species.forEach((specie) => {
+      animals[`${specie.name}`] = specie.residents.length;
+    });
+    return animals;
+  }
+  const reducedAnimal = Object.entries(animal).reduce((acc, curr) => acc.concat(curr));
+  if (reducedAnimal.length === 2) {
+    return species.find((specie) => specie.name === reducedAnimal[1]).residents.length;
+  }
+  if (reducedAnimal.length === 4) {
+    return species.find((specie) => specie.name === reducedAnimal[1]).residents
+      .filter((resident) => resident.sex === reducedAnimal[3]).length;
+  }
+};
 
-// console.log(countAnimals({ specie: 'penguins' }));
-
-// module.exports = countAnimals;
-
-// const flatten = (array) => array.reduce( (acc, curr) => acc.concat(curr) );
+module.exports = countAnimals;
