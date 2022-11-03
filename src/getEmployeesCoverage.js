50bb5c814d56904bb30a54b05fe3fb0f774caa2c
cunhavcs <cunha.vcs@gmail.com>
2022-11-03 11:57:44 -0300

    Resolve requisito 11 - Implementa a função getEmployeesCoverage

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,72 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
-}
+const { species, employees } = data;
+const errorMessage = 'Informações inválidas';
+
+const getSpecieByArrayId = (speciesIds) => {
+  const arrayOfSpeciesNames = [];
+  speciesIds.forEach((specieId) => {
+    arrayOfSpeciesNames.push(species.find((specie) => specie.id === specieId).name);
+  });
+  return arrayOfSpeciesNames;
+};
+
+const getLocationByArrayId = (speciesIds) => {
+  const arrayOfSpeciesLocations = [];
+  speciesIds.forEach((specieId) => {
+    arrayOfSpeciesLocations.push(species.find((specie) => specie.id === specieId).location);
+  });
+  return arrayOfSpeciesLocations;
+};
+
+const searchByName = (firstOrLastName) => {
+  const employeeInfo = employees.find((employee) => employee.firstName === firstOrLastName.name
+   || employee.lastName === firstOrLastName.name);
+  if (employeeInfo === undefined) {
+    throw new Error(errorMessage);
+  }
+  const formattedEmployeeInfo = {
+    id: employeeInfo.id,
+    fullName: `${employeeInfo.firstName} ${employeeInfo.lastName}`,
+    species: getSpecieByArrayId(employeeInfo.responsibleFor),
+    locations: getLocationByArrayId(employeeInfo.responsibleFor),
+  };
+  return formattedEmployeeInfo;
+};
+
+const searchById = (idNumber) => {
+  const employeeInfo = employees.find((employee) => employee.id === idNumber.id);
+  if (employeeInfo === undefined) {
+    throw new Error(errorMessage);
+  }
+  const formattedEmployeeInfo = {
+    id: employeeInfo.id,
+    fullName: `${employeeInfo.firstName} ${employeeInfo.lastName}`,
+    species: getSpecieByArrayId(employeeInfo.responsibleFor),
+    locations: getLocationByArrayId(employeeInfo.responsibleFor),
+  };
+  return formattedEmployeeInfo;
+};
+
+const displaysInfoAllEmployees = () => {
+  const employeesInfo = [];
+  employees.forEach((employee) => {
+    employeesInfo.push(searchByName({ name: employee.firstName }));
+  });
+  return employeesInfo;
+};
+
+const getEmployeesCoverage = (dataForSearch) => {
+  if (dataForSearch === undefined) {
+    return displaysInfoAllEmployees();
+  }
+  if (Object.keys(dataForSearch)[0] === 'name') {
+    return searchByName(dataForSearch);
+  }
+  if (Object.keys(dataForSearch)[0] === 'id') {
+    return searchById(dataForSearch);
+  }
+  throw new Error(errorMessage);
+};
 
 module.exports = getEmployeesCoverage;
